e27b17d7653f7a4754d05162fc541f424db8cb70
AliYH15 <ali.hasan.haydar@gmail.com>
2023-11-21 13:39:27 +0200

    Updated

--- deleteInter.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("conn.php");
-mysqli_query($con, "DELETE FROM books WHERE bookID = '$_GET[id]'");
-header("Location: select.php");
-?>
-
--- deletecartInter.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("conn.php");
-mysqli_query($con, "DELETE FROM cart WHERE title ='$_GET[title]'");
-header("Location: index.php");
-?>
-
